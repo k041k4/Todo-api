9e91370360936f766d18794086ce9d90f8d37dc5
Vladimir Baranek <k041k4@gmail.com>
2016-04-22 14:48:50 -0700

    Todo IDs

--- server.js
+++ server.js
@@ -1,10 +1,58 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+  id: 1,
+  description: 'zavolaj mame',
+  completed: false
+}, {
+  id: 2,
+  description: 'chod do obchodu',
+  completed: false
+} ,{
+  id: 3,
+  description: 'zobud sa',
+  completed: true
+}];
 
-app.get('/', function (request, response) {
+app.get('/', function(request, response) {
   response.send('Todo API root');
 });
+
+// All todos
+app.get('/todos', function(request, response) {
+  response.json(todos);
+});
+
+// Specific todos
+app.get('/todos/:id', function(request, response) {
+  var resTodo;
+  var reqId = parseInt(request.params.id,10);
+
+// todos.forEach( function(todo) {
+//   if (todo.id === reqId) {
+//     resTodo = todo;
+//   }
+// });
+// if (resTodo) {
+//   response.json(resTodo);
+// } else {
+//   response.status(404).send();
+// }
+
+  for (var i = 0; i < todos.length; i++) {
+    if (todos[i].id === reqId) {
+      resTodo = true;
+    }
+  }
+  if (resTodo) {
+    response.json(todos[reqId - 1]);
+//    response.send('To Do #' + request.params.id + '/n' + 'EXISTS!');
+  } else {
+    response.status(404).send();
+  }
+});
+
 
 app.listen(PORT, function() {
   console.log('Express listening on port ' + PORT);
